01c5d7937e68ab952855820f30529609c4e775bf
BlackGostt6 <gabrielsam.276@gmail.com>
2026-01-11 16:13:32 -0300

    corrigindo erro

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const botao = document.getElementById("botao");
 const botao2 = document.getElementById("botao2");
 
 botao.addEventListener("click", function() {
-    window.location.href = "../contact/index.html";
+    window.location.href = "contact/index.html";
 });
 
 botao2.addEventListener("click", function() {
-    window.location.href = "../services/index.html";
+    window.location.href = "services/index.html";
 });
